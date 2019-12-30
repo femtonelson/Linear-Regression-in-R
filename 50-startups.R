fdb1bbf44898a82e3cbec20c54b5918488ab01be
Nelson F <57707061+femtonelson@users.noreply.github.com>
2019-12-30 15:51:21 +0100

    Update 50-startups.R

--- 50-startups.R
+++ 50-startups.R
@@ -1,1 +1,7 @@
-#Dat source : https://raw.githubusercontent.com/tuyenhavan/Statistics/Dataset/50-Startups.csv
+#Data source : https://raw.githubusercontent.com/tuyenhavan/Statistics/Dataset/50-Startups.csv
+
+#Importing the data
+startup_data <- read.csv("50-startups.csv")
+
+# Exploratory Data Analsis
+str(startup_data)
