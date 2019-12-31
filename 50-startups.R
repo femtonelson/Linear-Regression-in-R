8da17f5c19e65f794a7d5cebc38ccd643464ac59
Nelson F <57707061+femtonelson@users.noreply.github.com>
2019-12-31 11:52:27 +0100

    Update 50-startups.R

--- 50-startups.R
+++ 50-startups.R
@@ -1,7 +1,20 @@
-#Data source : https://raw.githubusercontent.com/tuyenhavan/Statistics/Dataset/50-Startups.csv
-
 #Importing the data
 startup_data <- read.csv("50-startups.csv")
 
-# Exploratory Data Analsis
+#Visualizing the data
 str(startup_data)
+
+#Given the relative small size of the dataset, cross-validation will not be performed.
+
+# Evaluation of correlation matrix between numerical variables
+startup_numeric <- startup_data %>% select(R.D.Spend, Administration, Marketing.Spend, Profit)
+cor(startup_numeric)
+
+# Analysis of variance between Profit and State (Single factor)
+startup_factor <- startup_data %>% select(State, Profit)
+test_aov <- aov(Profit~State, data=startup_factor)
+summary(test_aov)
+
+#Model fitting
+my_model = lm(Profit~R.D.Spend, Administration, Marketing.Spend, data=startup_data)
+summary(my_model)
